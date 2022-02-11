c91bd6e8cd2fb1ba1deb2440e0d66cf0b824e991
Nikita Dudnik <nikdudnik@gmail.com>
2022-02-11 20:40:41 +0100

    Improve shape

--- packages/3d-mess/src/index.tsx
+++ packages/3d-mess/src/index.tsx
@@ -10,46 +10,53 @@ function profile(SEGMENTS: number) {
   const K1 = Math.random() * 3 + 1
   const K2 = Math.random() + 0.1
   const K3 = (Math.random() * 15 + 5) * 0.1
+  const K4 = Math.random() * 30 + 3
 
   const f1 = (k: number, i: number) =>
-    Math.sin((k * (i * step) * Math.PI) / 180)
+    (Math.sin((k * (i * step) * Math.PI) / 180) + 1) / 2
 
   return Array(SEGMENTS)
     .fill([])
-    .map((_, i) => f1(K1, i) * f1(K2, i) + f1(K3, i))
+    .map((_, i) => f1(K1, i) * f1(K2, i) + f1(K3, i) + f1(K4, i) + 0.2)
 }
 
 function makePoints2d(HEIGHT, SEGMENTS) {
   return [
     new Vector2(0, 0),
-    ...profile(SEGMENTS).map(
-      (x, y) => new Vector2(x - 2, (y * HEIGHT) / SEGMENTS),
-    ),
+    ...profile(SEGMENTS).map((x, y) => new Vector2(x, (y * HEIGHT) / SEGMENTS)),
     new Vector2(0, HEIGHT),
   ]
 }
 
 function App() {
   const SEGMENTS = 360
-  const HEIGHT = 10
+  const HEIGHT = 5
 
-  const [points2d, setPoints2d] = useState(
-    makePoints2d(HEIGHT, SEGMENTS),
-  )
+  const [points2d, setPoints2d] = useState(makePoints2d(HEIGHT, SEGMENTS))
 
   return (
-    <Canvas camera={{ position: [0, 0, -10] }}>
+    <Canvas shadows={true} camera={{ position: [0, 0, -6] }}>
       <OrbitControls autoRotate={true} autoRotateSpeed={-10} />
       <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <group position={[0, -5, 0]}>
+      <pointLight
+        position={[10, 10, 10]}
+        castShadow
+        shadowMapWidth={1024}
+        shadowMapHeight={1024}
+      />
+      <group position={[0, -2.5, 0]}>
         <mesh
-          onDoubleClick={() =>
-            setPoints2d(makePoints2d(HEIGHT, SEGMENTS))
-          }
+          castShadow
+          receiveShadow
+          onDoubleClick={() => setPoints2d(makePoints2d(HEIGHT, SEGMENTS))}
         >
           <latheGeometry args={[points2d, 72]} />
-          <meshStandardMaterial color={'hotpink'} />
+          <meshPhysicalMaterial
+            color={'hotpink'}
+            metalness={0.3}
+            roughness={0.5}
+            clearcoat={0}
+          />
         </mesh>
       </group>
     </Canvas>
